05a4a7175e0140ec56426320196fc94c82ae39e5
lauramagallanes <pasocenturiontours@gmail.com>
2022-09-26 17:49:57 +0000

    first commit excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let firstPart = who[Math.floor(Math.random() * who.length)];
+
+  let secondPart = what[Math.floor(Math.random() * what.length)];
+
+  let thirdPart = when[Math.floor(Math.random() * when.length)];
+
+  let newExcuse = firstPart + " " + secondPart + " " + thirdPart;
+
+  document.getElementById("excuse").innerHTML = newExcuse;
 };
